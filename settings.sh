9cbc3d224fde11138c33235935bea7765202dc28
dlovell <dlovell@gmail.com>
2013-12-12 15:46:44 -0500

    make settings.sh more general

--- settings.sh
+++ settings.sh
@@ -1,7 +1,9 @@
 project_name=vm-install-archetype-virtualbox
+osname=ubuntu-12.04.2-server
+preseed_filename=${osname}-preseed.cfg
 project_dir=$project_name
 ovf_full_path="${project_dir}"/"${project_name}".ovf
 port_number=2222
-username=bigdata
+username=$(grep '^d-i passwd/username string' $preseed_filename | awk '{print $NF}')
 rsa_key_filename=vm_guest_id_rsa
-packer_config_filename=vbox_ubuntu-12.04.2-server-amd64_packer_config.json
+packer_config_filename=vbox_${osname}-amd64_packer_config.json
