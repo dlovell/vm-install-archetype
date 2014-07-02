a995f1242f0fdb335d745a65f0e971a2d56a9288
dlovell <dlovell@gmail.com>
2014-07-02 12:55:20 -0400

    BUGFIX: make tar command work

--- create_tgz.sh
+++ create_tgz.sh
@@ -6,5 +6,5 @@ source $1
 task="creation of VM tgz"
 echo `date`: Starting $task
 cp VM_README.md $rsa_key_filename $output_dir
-tar cvfz $tgz_filename -C ${output_dir} $(find ${output_dir} -type f)
+tar cvfz $tgz_filename $output_dir
 echo `date`: Done $task
