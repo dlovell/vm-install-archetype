dbcfbe7ff4a7c6cf6466df62432e6a36bd7f89a8
dlovell <dlovell@gmail.com>
2014-07-07 08:15:14 -0400

    do shutdown inside of VM rather than external command
    in effort to get chown of project dir to stick

--- provision_project_in_guest.sh
+++ provision_project_in_guest.sh
@@ -4,17 +4,21 @@ source $1
 source helpers.sh
 
 
-function provision_project {
-	vm-connect "echo $username | sudo -S bash provision_project.sh"
-}
-
 function import_boot {
 	vm-import
 	vm-boot
 }
 
-function poweroff_export_unregister_delete {
-	vm-poweroff
+function provision_project {
+	vm-connect "echo $username | sudo -S bash provision_project.sh"
+}
+
+function vm-shutdown {
+	vm-connect "echo $username | sudo -S shutdown -h now"
+}
+
+function shutdown_export_unregister_delete {
+	vm-shutdown
 	# must delete files first; not allowed to clobber
 	rm $ovf_full_path
 	rm ${ovf_full_path%.ovf}-disk*vmdk
@@ -27,5 +31,5 @@ task="provisioning of project in guest"
 echo `date`: Starting $task
 import_boot
 provision_project
-poweroff_export_unregister_delete
+shutdown_export_unregister_delete
 echo `date`: Done $task
