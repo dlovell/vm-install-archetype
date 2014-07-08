83f45f66ea76aec530b64cfb376e9bb32f915b24
dlovell <dlovell@gmail.com>
2014-07-08 07:13:07 -0400

    make functions

--- CrossCat/provision_project.sh
+++ CrossCat/provision_project.sh
@@ -1,15 +1,28 @@
-set -e
+set -eu
 
 
 repo_base_url=https://github.com/mit-probabilistic-computing-project/crosscat
-project_name=~/$(basename $repo_base_url)
+project_dir=~/$(basename $repo_base_url)
 
 
-git clone $repo_base_url $project_name
-cd $project_name
-python setup.py install
-python setup.py build_ext --inplace
-if [[ ! -z $SUDO_USER ]]; then
-	chown -R $SUDO_USER:$SUDO_USER $project_name
-	sleep 4 # else chown doesn't seem to take
-fi
+function get_crosscat {
+	git clone $repo_base_url $project_dir
+}
+
+function make_crosscat {
+	cd $project_dir
+	python setup.py install
+	python setup.py build_ext --inplace
+}
+
+function chown_crosscat {
+	if [[ ! -z $SUDO_USER ]]; then
+		chown -R $SUDO_USER:$SUDO_USER $project_dir
+		sleep 4 # else chown doesn't seem to take
+	fi
+}
+
+
+get_crosscat
+make_crosscat
+chown_crosscat
