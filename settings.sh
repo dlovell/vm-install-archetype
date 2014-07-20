a3b25faa072e25585f2f158c86ebf3669bfab147
dlovell <dlovell@gmail.com>
2014-07-20 16:12:24 -0400

    default to ubuntu 12.04.2

--- settings.sh
+++ settings.sh
@@ -4,8 +4,9 @@ project_name=vm-install-archetype-virtualbox
 rsa_key_filename=vm_guest_id_rsa
 guest_script_dir=guest-script
 #
-# osname=ubuntu-12.04.2-server
-osname=ubuntu-14.04-server-amd64
+osname=ubuntu-12.04.2-server
+# osname=ubuntu-13.10-desktop-amd64
+# osname=ubuntu-14.04-server-amd64
 packer_config_filename=config/vbox_${osname}_packer_config.json
 # preseed filename can't have dashes, '-', to be served up by packer
 preseed_template=config/${osname}_preseed.cfg
